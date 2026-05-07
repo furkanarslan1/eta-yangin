1177aa86b668b80fe3d39eecfa7865f6bf16c0d5
furkanarslan1 <furkanarslandev@gmail.com>
2026-05-07 15:00:29 +0300

    robots

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
+      disallow: "/cgi-bin",
     },
     sitemap: "https://www.etayangin.com.tr/sitemap.xml",
     host: "https://www.etayangin.com.tr",
