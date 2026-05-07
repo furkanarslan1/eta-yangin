a074d0fb2c524edbe6c9c71d3725f27e8b669e5d
furkanarslan1 <furkanarslandev@gmail.com>
2026-05-07 14:06:51 +0300

    feat:add hero new service

--- lib/constants/hero.ts
+++ lib/constants/hero.ts
@@ -1,6 +1,17 @@
 export const heroSlides = [
   {
+    image: "/optimized/hero/oda-sizdirmazlik-hero.webp",
+    width: 1280,
+    height: 683,
+    alt: "Oda Sızdırmazlık Testi",
+    title: "Oda sızdırmazlık testi",
+    description:
+      "Gazlı söndürme yapılan mahallerde gazın gerekli süre korunmasını doğrulayan oda sızdırmazlık testi hizmeti sunuyoruz.",
+  },
+  {
     image: "/optimized/hero/eta-gaz-hero.webp",
+    width: 1280,
+    height: 853,
     alt: "Gaz Söndürme Sistemleri",
     title: "Gazlı Yangın Söndürme",
     description:
@@ -8,6 +19,8 @@ export const heroSlides = [
   },
   {
     image: "/optimized/hero/itfaiye-hero.webp",
+    width: 1280,
+    height: 683,
     alt: "İtfaiye Malzemeleri",
     title: "İtfaiye Malzemeleri",
     description:
@@ -15,6 +28,8 @@ export const heroSlides = [
   },
   {
     image: "/optimized/hero/davlumbaz-hero.webp",
+    width: 1280,
+    height: 683,
     alt: "Davlumbaz Söndürme Sistemleri",
     title: "Davlumbaz Söndürme Sistemleri",
     description:
